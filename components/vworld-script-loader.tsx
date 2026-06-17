29d4f7314b382baf1ba6d9d59b0fd7b6927eed3b
h083s25039 <h083s25039@gw1.kr>
2026-06-17 15:49:17 +0000

    debug: VWorld 스크립트 로더에 진단용 콘솔 로그 추가

--- components/vworld-script-loader.tsx
+++ components/vworld-script-loader.tsx
@@ -10,15 +10,24 @@ declare global {
 }
 
 export function VWorldScriptLoader() {
+  console.log("VWorldScriptLoader: 컴포넌트 렌더링됨. 스크립트 로딩 시도...");
+
   return (
     <Script
       strategy="beforeInteractive"
       src={`https://map.vworld.kr/js/webglMapInit.js.do?version=3.0&apiKey=${process.env.NEXT_PUBLIC_VWORLD_API_KEY}`}
       onLoad={() => {
+        console.log("VWorldScriptLoader: 스크립트 로딩 성공 (onLoad 실행됨).");
         // 스크립트 로드가 완료되면 전역 initMap 함수를 호출합니다.
         if (window.initMap) {
+          console.log("VWorldScriptLoader: window.initMap 함수를 찾음. 이제 호출합니다.");
           window.initMap()
+        } else {
+          console.error("VWorldScriptLoader: 스크립트는 로딩됐지만 window.initMap 함수를 찾을 수 없습니다!");
         }
+      }}
+      onError={(e) => {
+        console.error("VWorldScriptLoader: VWorld 스크립트 로딩 실패.", e);
       }}
     />
   )
